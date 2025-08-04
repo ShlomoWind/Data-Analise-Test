46ad6b082f3dc9efa7a2012b3a76d513d2128638
Shlomo <sh.z.wind@gmail.com>
2025-08-04 10:03:06 +0300

    creating the department Explore and preparing the infrastructure for functions

---
+++ src/data_investigation.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+class Explore:
+    def __init__(self,csv_file_path):
+        self.data = csv_file_path
+
+#tweets counter by category and total
+    def number_of_tweets(self):
+        pass
+
+#the mean length of tweets by each category and total
+    def length_of_tweets(self):
+        pass
+
+#the three longest tweets by each category
+    def three_longest_tweets(self):
+        pass
+
+#the ten most common words in all tweets
+    def ten_most_common_words(self):
+        pass
+
+#how many words in capital letters by each category and total
+    def number_of_capital_letters(self):
+        pass
