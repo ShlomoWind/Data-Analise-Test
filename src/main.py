c5ee3e7fd056c1f6f760b0962069ad6ac1523b3e
Shlomo <sh.z.wind@gmail.com>
2025-08-04 17:28:54 +0300

    The logic works, the files are created, improvements are still being loaded

--- src/main.py
+++ src/main.py
@@ -14,11 +14,11 @@ result["longest_3_tweets"] = explore.three_longest_tweets()
 result["common_words"] = explore.ten_most_common_words()
 result["uppercase_words"] = explore.number_of_capital_letters()
 
-export_result = ExporterJson(result,"results/json.results")
+export_result = ExporterJson(result,"../results/results.json")
 export_result.export_to_json()
 
 cleaner = Cleaner(df,'Text','Biased')
-cleaned_df = cleaner.drop_columns().clean_text().convert_to_lower().remove_unclassified()
+cleaned_df = cleaner.drop_columns().clean_text().convert_to_lower().remove_unclassified().get_clean_df()
 
-export_cleaned_data = ExporterCsv(cleaned_df,"results/csv.cleaned_dataset_tweets")
+export_cleaned_data = ExporterCsv(cleaned_df,"../results/tweets_dataset_cleaned.csv")
 export_cleaned_data.export_to_csv()
